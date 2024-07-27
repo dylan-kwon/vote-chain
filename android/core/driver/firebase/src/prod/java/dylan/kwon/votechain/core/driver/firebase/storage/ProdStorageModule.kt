c30c24cf69b473374a0f835c1d1a003c3145e8fb
dylan-kwon <dylan.kwon98@gmail.com>
2024-07-28 03:50:41 +0900

    [Android] Fix Name Error DevStorageModule -> ProdStorageModule

--- android/core/driver/firebase/src/prod/java/dylan/kwon/votechain/core/driver/firebase/storage/ProdStorageModule.kt
+++ android/core/driver/firebase/src/prod/java/dylan/kwon/votechain/core/driver/firebase/storage/ProdStorageModule.kt
@@ -10,7 +10,7 @@ import dagger.hilt.components.SingletonComponent
 
 @Module
 @InstallIn(SingletonComponent::class)
-object DevStorageModule {
+object ProdStorageModule {
 
     @Provides
     fun provideDevFireStore(): FirebaseStorage =
